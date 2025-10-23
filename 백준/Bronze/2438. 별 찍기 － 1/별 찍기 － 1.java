b2f77b5ebdb8d3bfe295fdb17bc78b77125a2788
jngbnss <jngbnmn@naver.com>
2025-10-23 20:44:33 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 176 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s ="";
+        for(int i=0;i<n;i++){
+            s+="*";
+            System.out.println(s);
+        }
+
+
+    }
+}
