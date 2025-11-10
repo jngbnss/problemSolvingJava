0f104fc498580917a35ef81da1dde1090b19cda4
jngbnss <jngbnmn@naver.com>
2025-11-10 09:49:15 +0900

    [Bronze V] Title: A+B - 5, Time: 204 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            if(a==0&&b==0) return;
+            System.out.println((a+b));
+        }
+    }
+}
